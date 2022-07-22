bf3eb75130bd708ffca6257eda7dd72258ee59e9
Evgenij <evgenij.voronov@icloud.com>
2022-07-22 22:20:57 +0300

    Initial Commit

---
+++ Enemy losses/Enemy_lossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enemy_lossesApp.swift
+//  Enemy losses
+//
+//  Created by Евгений Воронов on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Enemy_lossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
